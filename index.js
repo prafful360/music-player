5735b96491bf8d001dd41b790f587c4dd983e9ad
prafful360 <praffulranjan54@gmail.com>
2020-03-01 23:30:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const PORT = process.env.PORT || 4000;
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./config/db");
+const app = express();
+
+// Config DataBase and Mongoose
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(config.database, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch(err => {
+    console.log({
+      database_error: err
+    });
+  });
+// DB Configuration ends here!
+
+// Registering Cors
+app.use(cors());
+// Configure Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// configure body-parser ends here
+
+// making multer.js a static file
+app.use("/uploads", express.static("uploads"));
+
+app.use(morgan("dev")); // Config Morgan
+//define first route
+app.get("/", (req, res) => {
+  res.json("Hola MEVN devs....Assemble");
+});
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
